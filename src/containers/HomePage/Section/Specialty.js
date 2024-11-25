6ac705b7e7d8aaeeb802895e8898d98e9e0812cf
congcong222 <congnguyenvan522@gmail.com>
2024-11-25 13:12:45 +0700

    Xong giao diện 90 %

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -1,57 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './Specialty.scss'
-import { FormattedMessage } from 'react-intl'
-import './Specialty.scss';
-
+import { FormattedMessage } from 'react-intl' 
 import Slider from "react-slick";
-// Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import speciltyimg from '../../../assets/images/specialty/image.png'
+
 
 
 class Specialty extends Component {
   
     render() {
-        let settings = {
-          dots: false,
-          infinite: true,
-          speed: 500,
-          slidesToShow: 4,  //Hiển thị 4 cái trên 1 dòng,
-          slidesToScroll: 1, //Sẽ cuộn thêm 1 khi click
-        };
+        
         return (
-          <div className="section-specialty">
-            <div className="specialty-container">
-              <div className="specialty-header">
+          <div className="section-share section-specialty">
+            <div className="section-container">
+              <div className="section-header">
                 <span className='title-section'>Chuyên Khoa Phổ Biến</span>
                 <button className='btn-section'>Xem thêm</button>
               </div>
-              <div className="specialty-body">
-                <Slider {...settings}>
-                  <div className="specialty-customize">
-                    <div className="bg-image" />
+              <div className="section-body">
+                <Slider {...this.props.settings}>
+                  <div className="section-customize">
+                    <div className="bg-image section-specialty" />
                     <div className='specialty-text'>Cơ Xương Khớp 1</div>
                   </div>
-                  <div className="specialty-customize">
-                    <div className="bg-image" />
+                  <div className="section-customize">
+                    <div className="bg-image section-specialty" />
                     <div className='specialty-text'>Cơ Xương Khớp 2</div>
                   </div>
-                  <div className="specialty-customize">
-                    <div className="bg-image" />
+                  <div className="section-customize">
+                    <div className="bg-image section-specialty" />
                     <div className='specialty-text'>Cơ Xương Khớp 3</div>
                   </div>
-                  <div className="specialty-customize">
-                    <div className="bg-image" />
+                  <div className="section-customize">
+                    <div className="bg-image section-specialty" />
                     <div className='specialty-text'>Cơ Xương Khớp 4</div>
                   </div>
-                  <div className="specialty-customize">
-                    <div className="bg-image" />
+                  <div className="section-customize">
+                    <div className="bg-image section-specialty" />
                     <div className='specialty-text'>Cơ Xương Khớp 5</div>
                   </div>
-                  <div className="specialty-customize">
-                    <div className="bg-image" />
+                  <div className="section-customize">
+                    <div className="bg-image section-specialty" />
                     <div className='specialty-text'>Cơ Xương Khớp 6</div>
                   </div>
                 </Slider>
